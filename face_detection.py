0ff00e0190ebb9e01e48b2a9f09c8eeb8c00e866
Omar Padierna (Para11ax) <omar.ps16@gmail.com>
2025-10-28 15:34:35 -0400

    Adds logic to run E2E pipeline

--- face_detection.py
+++ face_detection.py
@@ -9,7 +9,7 @@ from facenet_pytorch import MTCNN
 from PIL import Image
 from matplotlib import pyplot as plt
 
-def face_detector(face_detection_args, image) -> List[torch.Tensor]:
+def face_detector(face_detection_args, image) -> List[tuple]:
     """
     Selects and runs the appropriate face detection algorithm.
 
@@ -18,7 +18,7 @@ def face_detector(face_detection_args, image) -> List[torch.Tensor]:
         image: Input image
 
     Returns:
-        Aligned faces from the selected detection algorithm
+        List of tuples (face_tensor, bounding_box) from the selected detection algorithm
     """
     if face_detection_args.mtcnn:
         is_debug_enabled = getattr(face_detection_args, 'debug', False)
@@ -26,26 +26,29 @@ def face_detector(face_detection_args, image) -> List[torch.Tensor]:
     elif face_detection_args.haar:
         return face_detection_haar(image)
 
-def face_detection_haar(image: Image.Image) -> List[torch.Tensor]:
+
+def face_detection_haar(image: Image.Image) -> List[tuple]:
     """Haar Cascade face detection stub.
 
     Args:
         image: PIL Image object
 
     Returns:
-        List of detected face tensors
+        List of tuples (face_tensor, bounding_box)
     """
     print("Running the haar flavor")
     return []
 
-def face_detection_mtcnn(image: Image.Image, is_debug_enabled:bool) -> List[torch.Tensor]:
+
+# Modify here to include bounding boxes. Output should be a list of tuples. Tensor + bounding box. This way they can be used for labeling.
+def face_detection_mtcnn(image: Image.Image, is_debug_enabled: bool) -> List[tuple]:
     """MTCNN face detection implementation.
 
     Args:
         image: PIL Image object
         is_debug_enabled: A flag that enables/disabled visualization and logs for debugging purposes
     Returns:
-        List of detected face tensors
+        List of tuples (face_tensor, bounding_box) where bounding_box is [x1, y1, x2, y2]
     """
     mtcnn = MTCNN(margin=20, keep_all=True, post_process=False, device='cuda')
 
@@ -54,7 +57,11 @@ def face_detection_mtcnn(image: Image.Image, is_debug_enabled:bool) -> List[torc
     plt.imshow(image)
     plt.axis('off')
 
-    # Detect faces
+    # Detect faces and get bounding boxes
+    # First call to get boxes and probs
+    boxes, probs = mtcnn.detect(image)
+
+    # Second call to get aligned face tensors
     faces = mtcnn(image)
 
     if is_debug_enabled:
@@ -70,8 +77,12 @@ def face_detection_mtcnn(image: Image.Image, is_debug_enabled:bool) -> List[torc
             for i, face in enumerate(faces):
                 axes[i].imshow(face.permute(1, 2, 0).int().numpy())
                 axes[i].axis('off')
-                axes[i].set_title(f'Face {i+1}')
+                axes[i].set_title(f'Face {i + 1}')
 
             fig.show()
-    #Note: MTCNN does face alignment automatically.
-    return faces if faces is not None else []
+    # Note: MTCNN does face alignment automatically.
+    if faces is not None and boxes is not None:
+        # Return list of tuples: (face_tensor, bounding_box)
+        return [(faces[i], boxes[i].tolist()) for i in range(len(faces))]
+    else:
+        return []
